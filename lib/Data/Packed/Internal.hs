f71a0a7e65f5fcd4e4e05a3cf114fc7d76419091
Alberto Ruiz <aruiz@um.es>
2007-06-14 16:50:31 +0000

    Internal.hs

---
+++ lib/Data/Packed/Internal.hs
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Packed.Internal
+-- Copyright   :  (c) Alberto Ruiz 2007
+-- License     :  GPL-style
+--
+-- Maintainer  :  Alberto Ruiz <aruiz@um.es>
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Reexports all internal modules
+--
+-----------------------------------------------------------------------------
+
+module Data.Packed.Internal (
+    module Data.Packed.Internal.Common,
+    module Data.Packed.Internal.Vector,
+    module Data.Packed.Internal.Matrix
+) where
+
+import Data.Packed.Internal.Common
+import Data.Packed.Internal.Vector
+import Data.Packed.Internal.Matrix
