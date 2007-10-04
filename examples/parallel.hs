3017c66ca1a77b55032f75acd529347648dfc59b
Alberto Ruiz <aruiz@um.es>
2007-10-04 18:48:49 +0000

    parallel multiply

--- examples/parallel.hs
+++ examples/parallel.hs
@@ -1,10 +1,27 @@
 import System(getArgs)
 import Numeric.LinearAlgebra
 import Control.Parallel.Strategies
+import System.Time
 
-work k = vectorMax s
-    where (_,s,_) = svd (ident k :: Matrix Double)
+inParallel = parMap rwhnf id
+
+parMul x y = fromBlocks [[ay],[by]]
+    where p = rows x `div` 2
+          a = takeRows p x
+          b = dropRows p x
+          [ay,by] = inParallel [a<>y,b<>y]
 
 main = do
-    args <- (read . head) `fmap` getArgs
-    print $ sum $ parMap rnf work args
+    n <- (read . head) `fmap` getArgs
+    let m = ident n :: Matrix Double
+    time $ print $ vectorMax $ takeDiag $ parMul m m
+    time $ print $ vectorMax $ takeDiag $ m <> m
+
+a = (2><3) [1..6::Double]
+b = (3><4) [1..12::Double]
+
+time act = do
+    t0 <- getClockTime
+    act
+    t1 <- getClockTime
+    print $ tdSec $ normalizeTimeDiff $ diffClockTimes t1 t0
