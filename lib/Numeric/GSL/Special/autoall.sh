bc2dcb1464ed2674e48bbb9372dc6135abe9bd1c
Alberto Ruiz <aruiz@um.es>
2008-02-21 18:50:18 +0000

    improved autoall

--- lib/Numeric/GSL/Special/autoall.sh
+++ lib/Numeric/GSL/Special/autoall.sh
@@ -5,23 +5,13 @@ function rep {
     cp /tmp/tmp-rep $3
 }
 
-#Exp.hs
-#  remove extern inline definition, qualify name
-#Coupling
-#  remove deprecated INCORRECT
-#Trig.hs
-#  qualify names
-#Legendre.hs
-#  remove extern inline
-#Log.hs
-#  remove extern inline, qualify name
 
 ./auto.hs airy
 rep ') where' ', Precision(..)\n) where' Airy.hs
 ./auto.hs bessel
 ./auto.hs clausen
 ./auto.hs coulomb
-runhaskell auto coupling
+./auto.hs coupling
 rep ', coupling_6j_INCORRECT_e\n, coupling_6j_INCORRECT\n' '' Coupling.hs
 ./auto.hs dawson
 ./auto.hs debye
@@ -29,7 +19,8 @@ rep ', coupling_6j_INCORRECT_e\n, coupling_6j_INCORRECT\n' '' Coupling.hs
 ./auto.hs elementary
 ./auto.hs ellint
 ./auto.hs erf
-#runhaskell auto exp
+./auto.hs exp gsl_sf_exp.h
+rep ', exp\n' ', Numeric.GSL.Special.Exp.exp\n' Exp.hs
 ./auto.hs expint
 ./auto.hs fermi_dirac
 ./auto.hs gamma
@@ -37,10 +28,13 @@ rep ', coupling_6j_INCORRECT_e\n, coupling_6j_INCORRECT\n' '' Coupling.hs
 ./auto.hs hyperg
 ./auto.hs laguerre
 ./auto.hs lambert
-#runhaskell auto legendre legendre.h
-#runhaskell auto log
+./auto.hs legendre gsl_sf_legendre.h
+./auto.hs log gsl_sf_log.h
+rep ', log\n' ', Numeric.GSL.Special.Log.log\n' Log.hs
 ./auto.hs pow_int
 ./auto.hs psi
 ./auto.hs synchrotron
-#runhaskell auto trig
+./auto.hs trig
+rep ', sin\n' ', Numeric.GSL.Special.Trig.sin\n' Trig.hs
+rep ', cos\n' ', Numeric.GSL.Special.Trig.cos\n' Trig.hs
 ./auto.hs zeta
