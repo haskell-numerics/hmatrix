42064526b944b6c0365105e4f3acc6bb1b38712e
Alberto Ruiz <aruiz@um.es>
2008-06-10 15:51:17 +0000

    other benchmark

--- examples/benchmarks.hs
+++ examples/benchmarks.hs
@@ -2,18 +2,20 @@
 
 -- compile as:
 -- ghc --make -O2 -optc-O2 -fvia-C  benchmarks.hs -o benchmarks
+-- ghc --make -O benchmarks.hs -o benchmarks
 
 import Numeric.LinearAlgebra
 import System.Time
 import System.CPUTime
 import Text.Printf
+import Data.List(foldl1')
 
 
 time act = do
     t0 <- getCPUTime
     act
     t1 <- getCPUTime
-    putStrLn $ show ((t1-t0) `div` 10^10) ++ " cs"
+    printf "%.2f CPU seconds\n" $ (fromIntegral ((t1 - t0) `div` (10^10)) / 100 :: Double) :: IO ()
 
 time' act = do
     t0 <- getClockTime
@@ -23,16 +25,18 @@ time' act = do
 
 --------------------------------------------------------------------------------
 
-main = sequence_ [bench1]
+main = sequence_ [bench1,bench2]
+
+w :: Vector Double
+w = constant 1 30000000
 
 bench1 = do
     putStrLn "sum of a vector with 30 million doubles:"
-    time $ printf "     BLAS: %.2f: " $ sumV sumVB 30000000
-    time $ printf "  Haskell: %.2f: " $ sumV sumVH 30000000
-    time $ printf "     BLAS: %.2f: " $ sumV sumVB 30000000
-    time $ printf "  Haskell: %.2f: " $ sumV sumVH 30000000
-
-sumV f n = f (constant (1::Double) n)
+    print$ vectorMax w -- evaluate it
+    time $ printf "     BLAS: %.2f: " $ sumVB w
+    time $ printf "  Haskell: %.2f: " $ sumVH w
+    time $ printf "     BLAS: %.2f: " $ sumVB w
+    time $ printf "  Haskell: %.2f: " $ sumVH w
 
 sumVB v = constant 1 (dim v) <.> v
 
@@ -42,3 +46,33 @@ sumVH v = go (d - 1) 0
        go :: Int -> Double -> Double
        go 0 s = s + (v @> 0)
        go !j !s = go (j - 1) (s + (v @> j))
+
+--------------------------------------------------------------------------------
+
+bench2 = do
+    putStrLn "-------------------------------------------------------"
+    putStrLn "multiplication of one million different 3x3 matrices"
+    putStrLn "from [[]]"
+    time $ print $ fun (10^6) rot'
+    putStrLn "from []"
+    time $ print $ fun (10^6) rot
+    print $ cos (10^6/2)
+
+
+rot' :: Double -> Matrix Double
+rot' a = matrix [[ c,0,s],
+                 [ 0,1,0],
+                 [-s,0,c]]
+    where c = cos a
+          s = sin a
+          matrix = fromLists
+
+rot :: Double -> Matrix Double
+rot a = (3><3) [ c,0,s
+               , 0,1,0
+               ,-s,0,c ]
+    where c = cos a
+          s = sin a
+
+fun n r = foldl1' (<>) (map r angles)
+    where angles = toList $ linspace n (0,1)
