eaa21e363a62274b3b457556d8f41b10cc335513
Alberto Ruiz <aruiz@um.es>
2014-04-23 14:18:43 +0200

    new version

--- reinstall.sh
+++ reinstall.sh
@@ -1,10 +1,10 @@
 cabal install --force-reinstalls
 
 cd packages/special
-cabal install --force-reinstalls
+cabal install --force-reinstalls --enable-documentation
 cd ../glpk
-cabal install --force-reinstalls
+cabal install --force-reinstalls --enable-documentation
 cd ../tests
-cabal install --force-reinstalls --enable-tests
+cabal install --force-reinstalls --enable-documentation --enable-tests
 cd ../..
 
