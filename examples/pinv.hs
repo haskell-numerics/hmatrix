f92699af9aa47eda9bd2c8a983487a706da75089
Alberto Ruiz <aruiz@um.es>
2015-10-02 10:39:21 +0200

    pinv

--- examples/pinv.hs
+++ examples/pinv.hs
@@ -1,20 +1,19 @@
 import Numeric.LinearAlgebra
-import Graphics.Plot
 import Text.Printf(printf)
 
-expand :: Int -> Vector Double -> Matrix Double
+expand :: Int -> Vector R -> Matrix R
 expand n x = fromColumns $ map (x^) [0 .. n]
 
-polynomialModel :: Vector Double -> Vector Double -> Int
-                -> (Vector Double -> Vector Double)
+polynomialModel :: Vector R -> Vector R -> Int
+                -> (Vector R -> Vector R)
 polynomialModel x y n = f where
-    f z = expand n z <> ws
+    f z = expand n z #> ws
     ws  = expand n x <\> y
 
 main = do
-    [x,y] <- (toColumns . readMatrix) `fmap` readFile "data.txt"
+    [x,y] <- toColumns <$> loadMatrix "data.txt"
     let pol = polynomialModel x y
     let view = [x, y, pol 1 x, pol 2 x, pol 3 x]
     putStrLn $ "  x      y      p 1    p 2    p 3"
     putStrLn $ format "  " (printf "%.2f") $ fromColumns view
-    mplot view
+
