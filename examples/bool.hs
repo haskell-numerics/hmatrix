04ec1d6b547d6c48506d66298f7d09f7de22c96e
Alberto Ruiz <aruiz@um.es>
2015-10-02 20:31:28 +0200

    bool

--- examples/bool.hs
+++ examples/bool.hs
@@ -1,17 +1,25 @@
 -- vectorized boolean operations defined in terms of step or cond
 
+{-# LANGUAGE FlexibleContexts #-}
+
 import Numeric.LinearAlgebra
 
 infix  4  .==., ./=., .<., .<=., .>=., .>.
 infixr 3  .&&.
 infixr 2  .||.
 
-a .<.  b = step (b-a)
-a .<=. b = cond a b 1 1 0
-a .==. b = cond a b 0 1 0
-a ./=. b = cond a b 1 0 1
-a .>=. b = cond a b 0 1 1
-a .>.  b = step (a-b)
+-- specialized for Int result
+cond'
+    :: (Element t, Ord t, Container c I, Container c t)
+    => c t -> c t -> c I -> c I -> c I -> c I
+cond' = cond
+
+a .<.  b = cond' a b 1 0 0
+a .<=. b = cond' a b 1 1 0
+a .==. b = cond' a b 0 1 0
+a ./=. b = cond' a b 1 0 1
+a .>=. b = cond' a b 0 1 1
+a .>.  b = cond' a b 0 0 1
 
 a .&&. b  = step (a*b)
 a .||. b  = step (a+b)
@@ -29,26 +37,22 @@ maxEvery a b = cond a b b b a
 
 clip a b x = cond y b y y b where y = cond x a a x x
 
-disp = putStr . dispf 3
+eye n = ident n :: Matrix R
 
-eye n = ident n :: Matrix Double
-row = asRow . fromList    :: [Double] -> Matrix Double
-col = asColumn . fromList :: [Double] -> Matrix Double
+m = (3><4) [1..] :: Matrix R
 
-m = (3><4) [1..] :: Matrix Double
-
-p = row [0,0,1,1]
-q = row [0,1,0,1]
+p = fromList [0,0,1,1] :: Vector I
+q = fromList [0,1,0,1] :: Vector I
 
 main = do
     print $ find (>6) m
-    disp $ assoc (6,8) 7 $ zip (find (/=0) (eye 5)) [10..]
-    disp $ accum (eye 5) (+) [((0,2),3), ((3,1),7), ((1,1),1)]
-    disp $ m .>=. 10  .||.  m .<. 4
-    (disp . fromColumns . map flatten) [p, q, p.&&.q, p .||.q, p `xor` q, p `equiv` q, p `imp` q]
+    disp 3 $ assoc (6,8) 7 $ zip (find (/=0) (eye 5)) [10..]
+    disp 3 $ accum (eye 5) (+) [((0,2),3), ((3,1),7), ((1,1),1)]
+    print $ m .>=. 10  .||.  m .<. 4
+    (print . fromColumns) [p, q, p.&&.q, p .||.q, p `xor` q, p `equiv` q, p `imp` q]
     print $ taut $ (p `imp` q ) `equiv` (no q `imp` no p)
     print $ taut $ (xor p q) `equiv` (p .&&. no q .||. no p .&&. q)
-    disp $ clip 3 8 m
-    disp $ col [1..7] .<=. row [1..5]
-    disp $ cond (col [1..3]) (row [1..4]) m 50 (3*m)
+    disp 3 $ clip 3 8 m
+    print $ col [1..7] .<=. row [1..5]
+    print $ cond (col [1..3]) (row [1..4]) m 50 (3*m)
 
